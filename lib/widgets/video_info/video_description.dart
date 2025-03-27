0814b83b6bef643536f8c63ea9384a43dc871e2b
C3B <joaodavisn@gmail.com>
2025-03-26 23:17:35 -0300

    description animation

--- lib/widgets/video_info/video_description.dart
+++ lib/widgets/video_info/video_description.dart
@@ -1,15 +1,84 @@
 import 'package:flutter/material.dart';
 
-class VideoDescription extends StatelessWidget {
+class VideoDescription extends StatefulWidget {
   final String text;
   final TextStyle? style;
   final int maxLines;
-  final TextOverflow overflow;
 
-  const VideoDescription({super.key, required this.text, this.style, this.maxLines = 2, this.overflow = TextOverflow.ellipsis});
+  const VideoDescription({
+    super.key, 
+    required this.text, 
+    this.style, 
+    this.maxLines = 2,
+  });
+
+  @override
+  State<VideoDescription> createState() => _VideoDescriptionState();
+}
+
+class _VideoDescriptionState extends State<VideoDescription> with SingleTickerProviderStateMixin {
+  bool _isExpanded = false;
+  late final AnimationController _animationController;
+  late final Animation<double> _scaleAnimation;
+  
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    
+    _scaleAnimation = TweenSequence<double>([
+      TweenSequenceItem(tween: Tween<double>(begin: 1.0, end: 1.03), weight: 30),
+      TweenSequenceItem(tween: Tween<double>(begin: 1.03, end: 1.0), weight: 70),
+    ]).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+  }
+  
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+  
+  void _toggleExpanded() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      if (_isExpanded) {
+        _animationController.forward(from: 0);
+      } else {
+        _animationController.forward(from: 0);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text(text, style: style ?? const TextStyle(color: Colors.white, fontSize: 13), maxLines: maxLines, overflow: overflow);
+    return GestureDetector(
+      onTap: _toggleExpanded,
+      child: AnimatedBuilder(
+        animation: _animationController,
+        builder: (context, child) {
+          return Transform.scale(
+            scale: _scaleAnimation.value,
+            alignment: Alignment.topLeft,
+            child: child,
+          );
+        },
+        child: AnimatedSize(
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeInOut,
+          child: Text(
+            widget.text,
+            style: widget.style ?? const TextStyle(color: Colors.white, fontSize: 13),
+            maxLines: _isExpanded ? null : widget.maxLines,
+            overflow: _isExpanded ? TextOverflow.visible : TextOverflow.ellipsis,
+          ),
+        ),
+      ),
+    );
   }
 }
