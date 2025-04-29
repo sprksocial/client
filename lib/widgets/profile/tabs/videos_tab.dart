02568199df7f5aeb6a1ff502fd0241cc562eb1c9
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2025-04-28 21:25:33 -0400

    fix background blur

--- lib/widgets/profile/tabs/videos_tab.dart
+++ lib/widgets/profile/tabs/videos_tab.dart
@@ -8,6 +8,7 @@ import '../../../services/auth_service.dart';
 import '../../../services/profile_service.dart';
 import '../../../widgets/video/video_item.dart';
 import '../../profile/profile_video_tile.dart';
+import '../../../services/feed_settings_service.dart';
 
 class VideosTab extends StatefulWidget {
   final String? did;
@@ -193,11 +194,14 @@ class _VideosTabState extends State<VideosTab> with AutomaticKeepAliveClientMixi
       PageRouteBuilder(
         opaque: true,
         pageBuilder:
-            (BuildContext context, _, __) => FeedScreen(
-              feedType: 0, // Use a custom feed type for profile videos
-              initialPosts: feedPosts,
-              initialIndex: index,
-              showBackButton: true,
+            (BuildContext context, _, __) => ChangeNotifierProvider<FeedSettingsService>.value(
+              value: FeedSettingsService(),
+              child: FeedScreen(
+                feedType: 0, // Use a custom feed type for profile videos
+                initialPosts: feedPosts,
+                initialIndex: index,
+                showBackButton: true,
+              ),
             ),
         transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
           const begin = Offset(0.0, 1.0);
