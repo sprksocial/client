545e02a8ae843e2eeceea4481c5ac8fb48863818
Roscoe Rubin-Rottenberg <roscoe@knotbin.com>
2025-06-15 20:16:19 -0400

    Update ci_pre_xcodebuild.sh

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,3 +1,10 @@
-#!/bin/zsh
+#!/bin/sh
 
+# Fail this script if any subcommand fails.
+set -e
+
+# Ensure Flutter is in PATH (should have been set up by ci_post_clone.sh)
+export PATH="$PATH:$HOME/flutter/bin"
+
+# Run build_runner
 dart run build_runner build --delete-conflicting-outputs
