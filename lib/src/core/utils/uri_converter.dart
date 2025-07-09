102fea2096fff069f4dac521f5ce044a0d348fe4
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2025-07-09 02:45:50 -0300

    sparky kkkkkkkkkkkkkkkkkk

--- lib/src/core/utils/uri_converter.dart
+++ lib/src/core/utils/uri_converter.dart
@@ -1,8 +1,3 @@
-// Copyright 2024 Sparky. All rights reserved.
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided the conditions.
-
-// 📦 Package imports:
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 class UriConverter implements JsonConverter<Uri, String> {
