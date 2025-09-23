f3f5085c08ee18d05e585fb8a938af979c1bd915
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2025-09-23 20:39:49 -0300

    action button

--- lib/src/core/design_system/components/atoms/buttons/action_button.dart
+++ lib/src/core/design_system/components/atoms/buttons/action_button.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:sparksocial/src/core/design_system/components/atoms/buttons/interactive_pressable.dart';
 import 'package:sparksocial/src/core/design_system/tokens/typography.dart';
 
-class ActionButton extends StatefulWidget {
+class ActionButton extends StatelessWidget {
   final Widget icon;
   final String label;
   final VoidCallback? onPressed;
@@ -17,75 +19,47 @@ class ActionButton extends StatefulWidget {
   });
 
   @override
-  State<ActionButton> createState() => _ActionButtonState();
-}
-
-class _ActionButtonState extends State<ActionButton> {
-  bool _isPressed = false;
-
-  void _onTapDown(TapDownDetails details) {
-    setState(() {
-      _isPressed = true;
-    });
-  }
-
-  void _onTapUp(TapUpDetails details) {
-    setState(() {
-      _isPressed = false;
-    });
-    widget.onPressed?.call();
-  }
-
-  void _onTapCancel() {
-    setState(() {
-      _isPressed = false;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final containerSize = _isPressed ? 40.5 : 45.0;
-    final blurAmount = _isPressed ? 2.25 : 2.5;
-    final opacity = _isPressed ? 0.6 : 1.0;
-    final totalHeight = _isPressed ? 54.5 : 59.0;
-    const double iconSize = 34;
+    const baseSize = 45.0;
+    const pressedScale = 0.9;
+    const iconSize = 34.0;
+
     final sizedIcon = SizedBox(
       width: iconSize,
       height: iconSize,
-      child: widget.icon,
+      child: icon,
     );
 
-    return GestureDetector(
-      onTapDown: _onTapDown,
-      onTapUp: _onTapUp,
-      onTapCancel: _onTapCancel,
-      child: SizedBox(
-        width: 45, // Base width is constant
-        height: totalHeight,
-        child: Opacity(
-          opacity: opacity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ClipOval(
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: blurAmount, sigmaY: blurAmount),
-                  child: Container(
-                    width: containerSize,
-                    height: containerSize,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withAlpha(25),
-                      shape: BoxShape.circle,
-                    ),
-                    child: Center(child: sizedIcon),
+    return SizedBox(
+      width: baseSize,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InteractivePressable(
+            onTap: onPressed,
+            pressedScale: pressedScale,
+            borderRadius: BorderRadius.circular(baseSize),
+            child: ClipOval(
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 2.5, sigmaY: 2.5),
+                child: Container(
+                  width: baseSize,
+                  height: baseSize,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withAlpha(25),
+                    shape: BoxShape.circle,
                   ),
+                  child: Center(child: sizedIcon),
                 ),
               ),
-              const SizedBox(height: 10),
-              Text(widget.label, textAlign: TextAlign.center, style: AppTypography.textSmallMedium),
-            ],
+            ),
           ),
-        ),
+          Text(
+            label,
+            textAlign: TextAlign.center,
+            style: AppTypography.textSmallMedium.copyWith(color: Colors.white),
+          ),
+        ],
       ),
     );
   }
