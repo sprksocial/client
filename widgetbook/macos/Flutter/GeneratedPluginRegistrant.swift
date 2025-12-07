e58636f527d7dcc9bd64f9ba4246623dfa2d62a9
Roscoe Rubin-Rottenberg <roscoe@knotbin.com>
2025-12-07 14:31:00 -0500

    justice for light mode

--- widgetbook/macos/Flutter/GeneratedPluginRegistrant.swift
+++ widgetbook/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
 import file_selector_macos
 import flutter_secure_storage_macos
 import fvp
@@ -19,6 +20,7 @@ import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FvpPlugin.register(with: registry.registrar(forPlugin: "FvpPlugin"))
